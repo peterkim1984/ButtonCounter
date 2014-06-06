4035cfbdfdfe052de5ce5f3b81c23e64326e9143
Peter <peterkim1984@gmail.com>
2014-06-05 19:53:43 -1000

    working counter

---
+++ button_counter.js
@@ -0,0 +1,19 @@
+(function() {
+
+  var counter = document.getElementById("counter");
+
+
+  var i = 0
+
+  counter.innerHTML = i;
+
+  count_but.onclick = function() {
+    i++;
+    counter.innerHTML = i;
+  }
+
+
+
+
+
+})();
